77716314ce1be630daa29017ecc75785ca7b3d61
2k-Aakaash <aakaashsasti@gmail.com>
2024-08-08 19:33:50 +0530

    Sidebar updated UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './Sidebar.css';
 
-const Sidebar = ({ open }) => (
-    <div className={`sidebar-container ${open ? 'open' : ''}`}>
-        <div className="sidebar-content">
-            <div className="sidebar-item">
-                <img src="src/assets/edit-icon.svg" alt="Diskette Icon" className="icon" />
-                <span className="text">Diskettes</span>
+const Sidebar = ({ open }) => {
+    const location = useLocation();
+
+    return (
+        <div className={`sidebar-container ${open ? 'open' : ''}`}>
+            <div className="sidebar-content">
+                <div className={`sidebar-item ${location.pathname === '/Diskette' ? 'active' : ''}`}>
+                    <img src="src/assets/edit-icon.svg" alt="Diskette Icon" className="icon" />
+                    <span className="text">Diskettes</span>
+                </div>
+                <div className="sidebar-item">
+                    <img src="src/assets/Notification-icon.svg" alt="Reminder Icon" className="icon" />
+                    <span className="text">Reminder (Coming Soon)</span>
+                </div>
             </div>
-            <div className="sidebar-item">
-                <img src="src/assets/Notification-icon.svg" alt="Reminder Icon" className="icon" />
-                <span className="text">Reminder (Coming Soon)</span>
+            <div className="settings-button">
+                <img src="src/assets/Settings-icon.svg" alt="Settings Icon" className="icon" />
+                <span className="text">Settings</span>
             </div>
         </div>
-        <div className="settings-button">
-            <img src="src/assets/Settings-icon.svg" alt="Settings Icon" className="icon" />
-            <span className="text">Settings</span>
-        </div>
-    </div>
-);
+    );
+};
 
 export default Sidebar;
