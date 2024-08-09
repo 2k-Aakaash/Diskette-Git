527217299793e1891906f622053fe0c4777338f9
2k-Aakaash <aakaashsasti@gmail.com>
2024-08-09 19:30:58 +0530

    Sidebar icons updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import './Sidebar.css';
 
+// Import the icons
+import disketteIcon from '../assets/edit-icon.svg';
+import reminderIcon from '../assets/Notification-icon.svg';
+import settingsIcon from '../assets/Settings-icon.svg';
+
 const Sidebar = ({ open }) => {
     const location = useLocation();
 
@@ -9,16 +14,16 @@ const Sidebar = ({ open }) => {
         <div className={`sidebar-container ${open ? 'open' : ''}`}>
             <div className="sidebar-content">
                 <div className={`sidebar-item ${location.pathname === '/Diskette' ? 'active' : ''}`}>
-                    <img src="src/assets/edit-icon.svg" alt="Diskette Icon" className="icon" />
+                    <img src={disketteIcon} alt="Diskette Icon" className="icon" />
                     <span className="text">Diskettes</span>
                 </div>
                 <div className="sidebar-item">
-                    <img src="src/assets/Notification-icon.svg" alt="Reminder Icon" className="icon" />
+                    <img src={reminderIcon} alt="Reminder Icon" className="icon" />
                     <span className="text">Reminder (Coming Soon)</span>
                 </div>
             </div>
             <div className="settings-button">
-                <img src="src/assets/Settings-icon.svg" alt="Settings Icon" className="icon" />
+                <img src={settingsIcon} alt="Settings Icon" className="icon" />
                 <span className="text">Settings</span>
             </div>
         </div>
