edf946d868d0884867b7f331adb00d85e1cdcfa9
2k-Aakaash <aakaashsasti@gmail.com>
2024-08-24 20:54:10 +0530

    Vite configjs updated

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
    server: {
     host: true, 
   },
-  base: '/',
+  base: '/Diskette',
   plugins: [react()],
   optimizeDeps: {
     include: ['jspdf', 'file-saver']
